6130694ebdb2088088aefbedd7a9a2e92cc1365f
Ahmet Sak <ahmett.sakk@gmail.com>
2023-08-05 20:21:46 +0300

    SQLite Update and Delete features tested

--- app/src/main/java/com/example/sqlitedeneme/MainActivity.kt
+++ app/src/main/java/com/example/sqlitedeneme/MainActivity.kt
@@ -13,10 +13,23 @@ class MainActivity : AppCompatActivity() {
             val myDatabase= this.openOrCreateDatabase("People", MODE_PRIVATE,null)
             myDatabase.execSQL("Create Table If Not Exists students (id INTEGER PRIMARY KEY, name VARCHAR, age INTEGER, studentNo INTEGER)")
 
-            //myDatabase.execSQL("INSERT INTO students (name, age, studentNo) VALUES ('Can', 18, 153)")
-            //myDatabase.execSQL("INSERT INTO students (name, age, studentNo) VALUES ('Temel', 18, 154)")
+           // myDatabase.execSQL("INSERT INTO students (name, age, studentNo) VALUES ('Can', 18, 153)")
+           // myDatabase.execSQL("INSERT INTO students (name, age, studentNo) VALUES ('Temel', 19, 154)")
+           // myDatabase.execSQL("INSERT INTO students (name, age, studentNo) VALUES ('Jesica', 19, 159)")
+           // myDatabase.execSQL("INSERT INTO students (name, age, studentNo) VALUES ('Julia', 18, 162)")
 
-            val cursor= myDatabase.rawQuery("SELECT * FROM students WHERE age=18",null)
+            //val cursor= myDatabase.rawQuery("SELECT * FROM students WHERE age= 18",null)
+            //val cursor= myDatabase.rawQuery("SELECT * FROM students WHERE name LIKE 'J%'",null)
+            //val cursor= myDatabase.rawQuery("SELECT * FROM students WHERE name LIKE '%n' ",null)
+            //val cursor= myDatabase.rawQuery("SELECT * FROM students WHERE studentNo>153 AND studentNo<160 ",null)
+
+            myDatabase.execSQL("UPDATE students SET name = 'John Wick' WHERE name = 'Temel'")
+            myDatabase.execSQL("UPDATE students SET studentNo = 160 WHERE name = 'Julia'")
+
+            myDatabase.execSQL("DELETE FROM students WHERE id=5")
+
+            val cursor= myDatabase.rawQuery("SELECT * FROM students ",null)
+
 
             val cursorNameRowIndex= cursor.getColumnIndex("name")
             val cursorAgeRowIndex= cursor.getColumnIndex("age")
@@ -25,10 +38,8 @@ class MainActivity : AppCompatActivity() {
 
 
             while (cursor.moveToNext()){
-                println("Öğrenci adı: ${cursor.getString(cursorNameRowIndex)}")
-                println("Öğrenci yaşı: ${cursor.getInt(cursorAgeRowIndex)}")
-                println("Öğrenci numarası: ${cursor.getInt(cursorStudentNoRowIndex)}")
-                println("id: ${cursor.getInt(cursorIdRowIndex)}")
+                println("Öğrenci id: ${cursor.getInt(cursorIdRowIndex)} adı: ${cursor.getString(cursorNameRowIndex)}  " +
+                        "yaşı: ${cursor.getInt(cursorAgeRowIndex)}, numarası: ${cursor.getInt(cursorStudentNoRowIndex)}")
             }
 
             cursor.close()
